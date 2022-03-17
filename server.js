54991adcc8ad1850eaa8ba9e0e5e291a8dc0b95a
Dhamareshwarakumar <damareshwarakumar@gmail.com>
2022-03-17 23:02:08 +0530

    Connected Frontend & Backend

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 const passport = require('passport');
 const cors = require('cors');
+const path = require('path');
 
 
 // Middlewares
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 
 // Configuration
@@ -25,6 +26,16 @@ const PORT = process.env.PORT || 3333;
 // Routes
 app.use('/api/admin', require('./routes/api/admin'));
 app.use('/api/events', require('./routes/api/events'));
+
+// Server Static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set Static Folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+};
 
 
 // Server
